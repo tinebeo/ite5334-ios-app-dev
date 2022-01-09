842f5dbad1b92bec0ab0056c5ddac95b08047ef2
Christine <caebeo@hotmail.com>
2022-01-09 01:04:52 -0500

    Delete Assignment4 directory

--- Assignment4/AnimeSchedule/AnimeSchedule/models/JikanService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  JikanService.swift
-//  animeschedule
-//
-//  Created by Christine Ebeo on 2021-12-29.
-//
-
-import Foundation
--- Assignment4/AnimeSchedule/AnimeSchedule/models/ScheduleModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ScheduleModel.swift
-//  animeschedule
-//
-//  Created by Christine Ebeo on 2021-12-30.
-//
-
-import Foundation
